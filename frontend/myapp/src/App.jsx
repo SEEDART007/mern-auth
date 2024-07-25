e4cda2fd7929ab99a3a4fa662af0ea8e8a92b976
seedart <siddharthachakraborty@gmail.com>
2024-07-25 23:24:35 +0530

    second-commit

--- frontend/myapp/src/App.jsx
+++ frontend/myapp/src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import About from './pages/About'
+import Profile from './pages/Profile'
 
 function App() {
   return (
-    <div className=' text-red-800'>
-      APPLICATION
-    </div>
+   <BrowserRouter>
+   <Routes>
+<Route path='/' element={<Home/>}/>
+<Route path='/signin' element={<Signin/>}/>
+<Route path='/signup' element={<Signup/>}/>
+<Route path='/about' element={<About/>}/>
+<Route path='/profile' element={<Profile/>}/>
+
+   </Routes>
+   
+   </BrowserRouter>
   )
 
 }
