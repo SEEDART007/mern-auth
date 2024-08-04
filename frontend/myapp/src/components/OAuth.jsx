6e34e4fe8f897e1ef14d31d782628342dd5cd51d
seedart <siddharthachakraborty@gmail.com>
2024-08-04 15:32:08 +0530

    completed profile page ui

--- frontend/myapp/src/components/OAuth.jsx
+++ frontend/myapp/src/components/OAuth.jsx
@@ -4,8 +4,10 @@ import { app } from '../firebase'
 import { signInFailure, signInStart, signInSuccess } from '../redux/user/userSlice'
 import { useDispatch } from 'react-redux'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 function OAuth() {
+  const navigate =useNavigate()
   const dispatch=useDispatch()
   const handleGoogle=async()=>{
     try {
@@ -21,6 +23,7 @@ function OAuth() {
       }
       const res=await axios.post('http://localhost:3000/api/auth/google',data)
       dispatch(signInSuccess(res))
+      navigate('/profile')
      
     } catch (error) {
       signInFailure(error)
--- frontend/myapp/src/pages/Profile.jsx
+++ frontend/myapp/src/pages/Profile.jsx
@@ -5,9 +5,25 @@ import { useSelector } from 'react-redux'
 function Profile() {
   const {currentUser}=useSelector((state)=>state.user)
   return (
-    <div>
-      
-      welcome! <span>{currentUser.data.username}</span>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-4xl font-semibold text-center mt-5'>Profile</h1>
+      <form className=' flex flex-col gap-4 '>
+        <img className='w-24 h-24 cursor-pointer rounded-full object-cover self-center mt-6' src={currentUser.data.profilePic} alt="" />
+        <input className=' bg-slate-200 h-11 p-3 rounded-md' defaultValue={currentUser.data.username} type="text" placeholder='Username' />
+        <input className=' bg-slate-200 h-11 p-3 rounded-md' defaultValue={currentUser.data.email} type="email" placeholder='Email' />
+        <input className=' bg-slate-200 h-11 p-3 rounded-md' type="password" placeholder='Password' />
+        <button className=' p-4 bg-slate-700 text-white rounded-lg'>Update</button>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span
+          className='text-red-700 cursor-pointer'
+        >
+          Delete Account
+        </span>
+        <span className='text-red-700 cursor-pointer'>
+          Sign out
+        </span>
+      </div>
     </div>
   )
 }
